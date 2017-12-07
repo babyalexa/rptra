2a04f330cc4da21793655b4626f806e192778f89
Heri <susantoh41@gmail.com>
2017-12-07 12:34:07 +0700

    Add some features

--- src/pages/details/details.ts
+++ src/pages/details/details.ts
@@ -1,8 +1,10 @@
-import { Component } from "@angular/core";
+import { Component, ViewChild, ElementRef } from "@angular/core";
 import { IonicPage, NavController, NavParams } from "ionic-angular";
 import { DrivePage } from "../drive/drive";
 import { StreetViewPage } from "../street-view/street-view";
 import { FormPage } from "../form/form";
+
+declare var google;
 
 /**
  * Generated class for the DetailsPage page.
@@ -22,8 +24,33 @@ export class DetailsPage {
     this.item = navParams.get("item");
   }
 
+  @ViewChild("map") mapRef: ElementRef;
+
   ionViewDidLoad() {
-    console.log("ionViewDidLoad DetailsPage");
+    this.showMap();
+  }
+
+  showMap() {
+    const location = new google.maps.LatLng(
+      this.item.location.latitude,
+      this.item.location.longitude
+    );
+
+    const options = {
+      center: location,
+      zoom: 15,
+      streetViewControl: false,
+      mapTypeId: "roadmap"
+    };
+
+    const map = new google.maps.Map(this.mapRef.nativeElement, options);
+    this.addMarker(location, map);
+  }
+  addMarker(position, map) {
+    return new google.maps.Marker({
+      position,
+      map
+    });
   }
 
   viewDrive(item) {
